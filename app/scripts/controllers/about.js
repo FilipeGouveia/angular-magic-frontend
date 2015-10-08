5893b01522b28c1e9db2e18ef654a7c30e1dc497
Filipe Gouveia <joaofrg@gmail.com>
2015-10-08 14:32:46 +0100

    Initialized project

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name magicUiApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the magicUiApp
+ */
+angular.module('magicUiApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name magicUiApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the magicUiApp
+ */
+angular.module('magicUiApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
